f5d0c6c12dc0ddb76a3c8b84f13d9973d2a98d50
asifmMustafa <asifmustafa.2o5@gmail.com>
2024-04-23 19:05:35 +0600

    header links moved to a list

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
 import React from "react";
 
 const Header = () => {
+  const links = [
+    "Home",
+    "Companies",
+    "About Us",
+    "Board of Directors",
+    "Contact Us",
+  ];
+
   return (
     <div className="absolute top-0 z-10 m-10 w-[95%] flex justify-center">
       <div className="w-[95%] p-3 flex justify-between rounded-lg bg-white">
         <img src="/images/logo-navbar.png" />
         <div className="flex flex-row items-center space-x-5 mr-4 font-outfit-light text-l tracking-wide">
-          <p className="hover:text-orange cursor-pointer">Home</p>
-          <p className="hover:text-orange cursor-pointer">Companies</p>
-          <p className="hover:text-orange cursor-pointer">About Us</p>
-          <p className="hover:text-orange cursor-pointer">Board of Directors</p>
-          <p className="hover:text-orange cursor-pointer">Contact Us</p>
+          {links.map((link, index) => (
+            <p key={index} className="hover:text-orange cursor-pointer">
+              {link}
+            </p>
+          ))}
         </div>
       </div>
     </div>
