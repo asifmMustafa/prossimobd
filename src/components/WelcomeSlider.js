c95141810eac82bdc99d89a151910f06d2df5ecd
asifmMustafa <asifmustafa.2o5@gmail.com>
2024-04-23 19:00:23 +0600

    header added

--- src/components/WelcomeSlider.js
+++ src/components/WelcomeSlider.js
@@ -1,19 +1,20 @@
 import React, { useRef } from "react";
 import Slider from "react-slick";
+import LeftArrow from "./LeftArrow";
+import RightArrow from "./RightArrow";
+import Header from "./Header";
 
 import "../slick-styles/slick.css";
 import "../slick-styles/slick-theme.css";
-import LeftArrow from "./LeftArrow";
-import RightArrow from "./RightArrow";
 
 const InnerContent = () => (
   <div className="w-2/3 pt-52 pb-32 pl-52">
-    <div className="inline-block py-[1px] px-3 rounded-full font-outfit-light text-l tracking-wide text-white bg-orange">
+    <p className="inline-block py-[1px] px-3 rounded-full font-outfit-light text-l tracking-wide text-white bg-orange">
       Where Innovation meets impact
-    </div>
-    <div className="py-5 block font-outfit-heavy text-8xl text-white">
+    </p>
+    <h1 className="py-5 block font-outfit-heavy text-8xl text-white">
       Welcome To MSN Group
-    </div>
+    </h1>
     <p className="w-2/3 font-outfit-light text-l tracking-wide text-white">
       Step into the world of MSN Group, whether it's revolutionizing real estate
       or introducing groundbreaking technologies, we pave the way for a future
@@ -38,6 +39,7 @@ const WelcomeSlider = () => {
 
   const sliderSettings = {
     dots: true,
+    arrows: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
@@ -45,7 +47,9 @@ const WelcomeSlider = () => {
   };
 
   return (
-    <div className="relative">
+    <section className="relative">
+      <Header />
+
       <Slider ref={sliderRef} {...sliderSettings}>
         <div className="p-10 bg-[url('/public/images/bg.png')] bg-cover">
           <InnerContent />
@@ -57,6 +61,7 @@ const WelcomeSlider = () => {
           <InnerContent />
         </div>
       </Slider>
+
       <div
         onClick={previous}
         className="absolute top-1/2 transform -translate-y-1/2 left-16"
@@ -69,7 +74,7 @@ const WelcomeSlider = () => {
       >
         <RightArrow />
       </div>
-    </div>
+    </section>
   );
 };
 
